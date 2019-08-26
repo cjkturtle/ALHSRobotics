01a2db9cd940faf4a7e31f9fbb5c82e9948d074e
cjkturtle <45407345+cjkturtle@users.noreply.github.com>
2019-08-25 20:38:25 -0400

    Update robot-config.h

--- ALHSRobotics-44012A/Current/robot-config.h
+++ ALHSRobotics-44012A/Current/robot-config.h
@@ -1,9 +1,12 @@
 vex::brain Brain;
 
-vex::motor LF = vex::motor(vex::PORT1,vex::gearSetting::ratio18_1,false);
-vex::motor LB = vex::motor(vex::PORT5,vex::gearSetting::ratio18_1,false);
+vex::motor LF = vex::motor(vex::PORT1,vex::gearSetting::ratio18_1,true);
+vex::motor LB = vex::motor(vex::PORT5,vex::gearSetting::ratio18_1,true);
 vex::motor RF = vex::motor(vex::PORT3,vex::gearSetting::ratio18_1,true);
 vex::motor RB = vex::motor(vex::PORT4,vex::gearSetting::ratio18_1,true);
+vex::motor Stacker = vex::motor(vex::port5,vex::gearSetting::ratio36_1,true);
+vex::motor IntakeL = vex::motor(vex::port6,vex::gearSetting::ratio36_1,true);
+vex::motor IntakeR = vex::motor(vex::port6,vex::gearSetting::ratio36_1,true);
 
 vex::controller Controller = vex::controller(vex::controllerType::primary);
 
