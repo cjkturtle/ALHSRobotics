0a9ae46c602f25f504478ed525b83a1d8c298305
cjkturtle <cjkturtle@icloud.com>
2019-08-25 21:26:55 -0400

    Update Main.cpp

--- ALHSRobotics-44012A/Current/Main.cpp
+++ ALHSRobotics-44012A/Current/Main.cpp
@@ -13,32 +13,40 @@ AUTO
 ********************************************************************/
 
 void autoLoop(){
-    
+
 }
- 
+
 /********************************************************************
-CONTROLLER 
+CONTROLLER
 ********************************************************************/
-    
+
 void ControllerLoop(){
      while (true) {
-         
+
+
+/********************************************************************
+FUNCTION BLOCKS
+*********************************************************************/
+
+
+
+
 /********************************************************************
 JOYSTICK SPEED CONTROL
-*********************************************************************/            
- 
+*********************************************************************/
+
  if(Controller.ButtonL1.pressing()){Speed = 1.3;}
  else if (Controller.ButtonUp.pressing()){Speed = 0.5;}
  else{Speed = 0.8;}
 
 /********************************************************************
-JOYSTICK 
+JOYSTICK
 ********************************************************************/
 
  JRX = Controller.Axis1.value(); //Joystick Right X
  JLX = Controller.Axis4.value(); //Joystick Left X
  JRY = Controller.Axis2.value(); //Joystick Right Y
-   
+
       if(fabs(JRX) || fabs(JRY) > t || fabs(JLX) > t){
          LF.spin(vex::directionType::fwd, ((JRY + JLX + JRX) / 2) * Speed, vex::velocityUnits::pct);
          LB.spin(vex::directionType::fwd, ((JRY + JLX - JRX) / 2) * Speed, vex::velocityUnits::pct);
@@ -49,27 +57,27 @@ JOYSTICK
            LF.stop(vex::brakeType::brake);
            LB.stop(vex::brakeType::brake);
            RF.stop(vex::brakeType::brake);
-           RB.stop(vex::brakeType::brake); 
-            } 
-   
+           RB.stop(vex::brakeType::brake);
+            }
+
 /********************************************************************
 CONTROLLER BUTTONS
 ********************************************************************/
-         
+
 
 
 //void screenLoop(){
    //while(true){
-   Brain.Screen.clearScreen(); 
+   Brain.Screen.clearScreen();
    Brain.Screen.printAt(2, 40, "LF Temp: %f" "%", LF.temperature(vex::percentUnits::pct));
-   Brain.Screen.printAt(2, 80, "LB Temp: %f" "%", LB.temperature(vex::percentUnits::pct));   
+   Brain.Screen.printAt(2, 80, "LB Temp: %f" "%", LB.temperature(vex::percentUnits::pct));
    Brain.Screen.printAt(2, 120, "RF Temp: %f" "%", RF.temperature(vex::percentUnits::pct));
-   Brain.Screen.printAt(2, 160, "RB Temp: %f" "%", RB.temperature(vex::percentUnits::pct)); 
-    vex::task::sleep(1);  
+   Brain.Screen.printAt(2, 160, "RB Temp: %f" "%", RB.temperature(vex::percentUnits::pct));
+    vex::task::sleep(1);
  }
 
 }
-    
+
     int main() {
         autoLoop();
         ControllerLoop();
@@ -77,4 +85,3 @@ CONTROLLER BUTTONS
         //Competition.drivercontrol(ControllerLoop);
          //while(true){vex::task::sleep(100);}
 }
-                               
