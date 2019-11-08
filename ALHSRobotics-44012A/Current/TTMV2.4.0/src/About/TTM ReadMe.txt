470860549cea7f1596fa5f78e80de7a51c87a1f3
cjkturtle <cjkturtle@icloud.com>
2019-11-07 20:47:38 -0500

    Added New File

---
+++ ALHSRobotics-44012A/Current/TTMV2.4.0/src/About/TTM ReadMe.txt
@@ -0,0 +1,37 @@
+Team: 44012A
+Competition: Tower Takeover
+File Author: Carter Knox
+Date: 11/4/2019
+Version: 2.3.2
+
+Features: 
+1. Initialization, driver control, and autonomous control periods.
+2. Full sensor debugging information displayed on V5 Brain screen.
+3. Support for the Vex vision sensor.
+4. Full library of preset functions that can be called throughout program.
+5. A custom replacement for the rotateFor function.
+6. Selectable autonomous loops choosen by the user during the robot initialization loop.
+7. Virtual field and robot mapped to follow autonomous code.
+8. Multiple autonomous programs are avaibable to choose from for each starting field position.
+
+Comments:
+1. Virtual field is not fully complete and is lacking some features in this version.
+2. The gyroscope is initialized but not yet used in this version.
+3. The vision sensor is initualized but not yet used in this version.
+
+Left stick forward/back
+Right stick turning
+CAR DRIVE ^
+
+Right Bumper top - intake
+Right bumper bottom - reverse intake
+
+A - DropStack();
+Left Bumper top - Swing Up | Pushing
+Left Bumper bottom - Swing Down | Pushing 
+X - Swing to location 0 | Push
+Y - Swing to location 1 | Push
+
+Up Arrow - 100% Speed | Toggle 
+Down Arrow - 33% Speed | Toggle
+Right Arrow && Left Arrow - Normal Speed | Toggle
---
+++ ALHSRobotics-44012A/Current/TTMV2.4.0/src/Controller_Profiles/CarterController.cpp
@@ -0,0 +1,133 @@
+#include "vex.h"
+using namespace vex;
+
+void CarterController(){
+
+  Brain.resetTimer(); 
+    gameStatus = 1;   
+    while (true) {
+    MainScreen();    
+      // Control setup for the drive speed.
+
+      if(Controller.ButtonLeft.pressing()){Speed = 1.5;}
+      else if (Controller.ButtonUp.pressing()){Speed = 0.5;}
+      else{Speed = 0.8;}
+
+      // Setup for the single joystick controller drive.
+
+        JRX = Controller.Axis1.value(); 
+        JRY = Controller.Axis2.value();
+
+      if(fabs(JRX) <= deadZone){JRX = 0;}
+      if(fabs(JRY) <= deadZone){JRY = 0;}
+      if(fabs(JRX) > threashold || fabs(JRY) > threashold){
+        LB.spin(vex::directionType::fwd, ((JRY + JRX) / 2) * Speed, vex::velocityUnits::pct);
+        RB.spin(vex::directionType::rev, ((JRY - JRX) / 2) * Speed, vex::velocityUnits::pct);
+      }
+      else{
+        Brake(); 
+      }
+
+
+      // Setup code for the controls on the controller.
+
+
+      // Control setup for the cube tray.
+
+      if(Controller.ButtonL1.pressing()){
+        SwingUp();
+      }
+      else if (Controller.ButtonL2.pressing()){
+        SwingDown();
+      }
+      else if(Controller.ButtonY.pressing()){ 
+        SwingToLocation(1);
+        Swing.stop(vex::brakeType::coast);
+      }
+      else if(Controller.ButtonX.pressing()){ 
+        SwingToLocation(0);
+        Swing.stop(vex::brakeType::coast);
+      }
+      else{
+        Swing.stop(vex::brakeType::coast);
+      }   
+
+      // Control setup for the cube intakes.
+
+      if(Controller.ButtonRight.pressing()){
+        IntakesMove(1);
+      }
+      else if(Controller.ButtonDown.pressing()){
+        IntakesMove(-0.5);
+      }
+      else{
+      // IntakesIdle();
+       IntakeL.stop(brakeType::hold);
+       IntakeR.stop(brakeType::hold);
+
+      }
+
+      // Control setup for the reverse four bar.
+
+      if(Controller.ButtonR1.pressing()){
+        R4BUp();
+      }
+      else if(Controller.ButtonR2.pressing()){
+        R4BDown();
+      }
+      else if(Controller.ButtonA.pressing()){
+        R4BToLocation(0);
+        R4BL.stop(vex::brakeType::hold);
+        R4BR.stop(vex::brakeType::hold);
+      }
+      else if(Controller.ButtonB.pressing()){
+        R4BToLocation(1);
+        R4BL.stop(vex::brakeType::hold);
+        R4BR.stop(vex::brakeType::hold);
+      }
+      else{
+        R4BL.stop(vex::brakeType::hold);
+        R4BR.stop(vex::brakeType::hold);
+      }
+
+      if(Controller.ButtonR2.pressing()){
+        DropStack();
+      }
+
+      if(IntakeTop.pressing()){
+        intakeTopStatus = true;
+      }
+      else{
+        intakeTopStatus = false;
+      }
+      if(SwingLimit.pressing()){
+        swingLimit = true;
+        Swing.resetRotation();
+      }
+      else{
+        swingLimit = false;
+      }
+      if((swingLimit) && (Controller.ButtonL1.pressing())){
+      Controller.rumble(".-."); 
+    } 
+    else {
+
+    }
+    if(intakeTopStatus){
+      Controller.rumble("...."); 
+    } 
+    else {
+
+    }
+
+    /*if(LB.temperature((vex::percentUnits::pct) > 65) || ((RB.temperature(vex::percentUnits::pct) > 65)){
+      Controller.Screen.clearScreen();
+      Controller.Screen.print("DRIVE MOTOR OVERTEMP");
+    }
+
+    else{
+
+    }
+    */
+    } 
+}    
