6b46b1425b633f26a0ffac7a23276194acacfaf1
cjkturtle <cjkturtle@icloud.com>
2019-11-07 21:20:46 -0500

    Update OtherController.cpp

--- ALHSRobotics-44012A/Current/TTMV2.4.0/src/Controller_Profiles/OtherController.cpp
+++ ALHSRobotics-44012A/Current/TTMV2.4.0/src/Controller_Profiles/OtherController.cpp
@@ -2,131 +2,98 @@
 using namespace vex;
 
 void OtherController(){
-Brain.resetTimer(); 
-  gameStatus = 1;   
-  while (true) {
-  MainScreen();    
-    // Control setup for the drive speed.
+  Brain.resetTimer(); 
+    gameStatus = 1;   
+    while (true) {
+    MainScreen();    
+      // Control setup for the drive speed.
 
-    if(Controller.ButtonLeft.pressing()){Speed = 1.5;}
-    else if (Controller.ButtonUp.pressing()){Speed = 0.5;}
-    else{Speed = 0.8;}
+      if(Controller.ButtonUp.pressing()){Speed = 1.5;}
+      else if (Controller.ButtonDown.pressing()){Speed = 0.5;}
+      else{Speed = 0.8;}
 
-    // Setup for the single joystick controller drive.
+      // Setup for the single joystick controller drive.
 
-      JRX = Controller.Axis1.value(); 
-      JRY = Controller.Axis2.value();
+        JRX = Controller.Axis1.value(); 
+        JLY = Controller.Axis3.value();
 
-    if(fabs(JRX) <= deadZone){JRX = 0;}
-    if(fabs(JRY) <= deadZone){JRY = 0;}
-    if(fabs(JRX) > threashold || fabs(JRY) > threashold){
-      LB.spin(vex::directionType::fwd, ((JRY + JRX) / 2) * Speed, vex::velocityUnits::pct);
-      RB.spin(vex::directionType::rev, ((JRY - JRX) / 2) * Speed, vex::velocityUnits::pct);
-    }
-    else{
-      Brake(); 
-    }
-     
-         
-    // Setup code for the controls on the controller.
-    
+      if(fabs(JRX) <= deadZone){JRX = 0;}
+      if(fabs(JRY) <= deadZone){JRY = 0;}
+      if(fabs(JRX) > threashold || fabs(JRY) > threashold){
+        LB.spin(vex::directionType::fwd, ((JLY + JRX) / 2) * Speed, vex::velocityUnits::pct);
+        RB.spin(vex::directionType::rev, ((JLY - JRX) / 2) * Speed, vex::velocityUnits::pct);
+      }
+      else{
+        Brake(); 
+      }
 
-    // Control setup for the cube tray.
 
-    if(Controller.ButtonL1.pressing()){
-      SwingUp();
-    }
-    else if (Controller.ButtonL2.pressing()){
-      SwingDown();
-    }
-    else if(Controller.ButtonY.pressing()){ 
-      SwingToLocation(1);
-      Swing.stop(vex::brakeType::coast);
-    }
-    else if(Controller.ButtonX.pressing()){ 
-      SwingToLocation(0);
-      Swing.stop(vex::brakeType::coast);
-    }
-    else{
-      Swing.stop(vex::brakeType::coast);
-    }   
-      
-    // Control setup for the cube intakes.
+      // Setup code for the controls on the controller.
 
-    if(Controller.ButtonRight.pressing()){
-      IntakesMove(1);
+
+      // Intakes
+
+      if(Controller.ButtonR1.pressing()){
+        IntakesMove(1);
+      }
+      else if (Controller.ButtonR2.pressing()){
+        IntakesMove(-0.5);
+      }
+      else{
+      }
+
+      // Swing
+      if(Controller.ButtonL1.pressing()){
+        SwingUp();
+      }
+      else if(Controller.ButtonL2.pressing()){
+        SwingDown();
+      }
+      else if(Controller.ButtonY.pressing()){ 
+        SwingToLocation(1);
+        Swing.stop(vex::brakeType::coast);
+      }
+      else if(Controller.ButtonX.pressing()){ 
+        SwingToLocation(0);
+        Swing.stop(vex::brakeType::coast);
+      }
+      else{
+        Swing.stop(vex::brakeType::coast);
+      }   
+
+
+      // Control setup for the cube intakes.
+      if(Controller.ButtonA.pressing()){
+        DropStack();
+      }
+
+
+      //Sensors
+      if(IntakeTop.pressing()){
+        intakeTopStatus = true;
+      }
+      else{
+        intakeTopStatus = false;
+      }
+      if(SwingLimit.pressing()){
+        swingLimit = true;
+        Swing.resetRotation();
+      }
+      else{
+        swingLimit = false;
+      }
+      if((swingLimit) && (Controller.ButtonL1.pressing())){
+      Controller.rumble(".-."); 
+    } 
+    else {
+
     }
-    else if(Controller.ButtonDown.pressing()){
-      IntakesMove(-0.5);
-    }
-    else{
-    // IntakesIdle();
-     IntakeL.stop(brakeType::hold);
-     IntakeR.stop(brakeType::hold);
+    if(intakeTopStatus){
+      Controller.rumble("...."); 
+    } 
+    else {
 
     }
 
-    // Control setup for the reverse four bar.
-
-    if(Controller.ButtonR1.pressing()){
-      R4BUp();
-    }
-    else if(Controller.ButtonR2.pressing()){
-      R4BDown();
-    }
-    else if(Controller.ButtonA.pressing()){
-      R4BToLocation(0);
-      R4BL.stop(vex::brakeType::hold);
-      R4BR.stop(vex::brakeType::hold);
-    }
-    else if(Controller.ButtonB.pressing()){
-      R4BToLocation(1);
-      R4BL.stop(vex::brakeType::hold);
-      R4BR.stop(vex::brakeType::hold);
-    }
-    else{
-      R4BL.stop(vex::brakeType::hold);
-      R4BR.stop(vex::brakeType::hold);
-    }
-
-    if(Controller.ButtonR2.pressing()){
-      DropStack();
-    }
-
-    if(IntakeTop.pressing()){
-      intakeTopStatus = true;
-    }
-    else{
-      intakeTopStatus = false;
-    }
-    if(SwingLimit.pressing()){
-      swingLimit = true;
-      Swing.resetRotation();
-    }
-    else{
-      swingLimit = false;
-    }
-    if((swingLimit) && (Controller.ButtonL1.pressing())){
-    Controller.rumble(".-."); 
-  } 
-  else {
-    
-  }
-  if(intakeTopStatus){
-    Controller.rumble("...."); 
-  } 
-  else {
-    
-  }
-
-  /*if(LB.temperature((vex::percentUnits::pct) > 65) || ((RB.temperature(vex::percentUnits::pct) > 65)){
-    Controller.Screen.clearScreen();
-    Controller.Screen.print("DRIVE MOTOR OVERTEMP");
-  }
-      
-  else{
-          
-  }
-  */
   } 
 }    
